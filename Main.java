a225f7cb3d3bde6391516979166fb7f78cd9bdc7
Richard C305 <113560965+rcui-admin@users.noreply.github.com>
2022-10-10 13:46:53 +0000

    Imported Scanner utility Created a Scanner object Created a String variable

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner word = new Scanner(System.in);  //Scanner object created
     System.out.println("Enter a word:");
+    String wordInput = word.nextLine(); //User input recorded to this variable
+    System.out.println(wordInput.length()); //Displays the length of the word
     
     //Your code will return the length of the entered String
   }
